5888c46fd6700428d258e7a1b1253830f8a12f06
printf-bomotayo <bomotayo2020@outlook.com>
2024-03-12 22:45:42 +0100

    Adding API documentation

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
+"""
+Database models.
+"""
+
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create save, and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the sytem."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()  ## this is used to  assign user manager in django.
+
+    USERNAME_FIELD = 'email' ## this is how to set the custom user model to use email as the authentication as against using Username which is the default that comes with Django user manager
